8fadbd053807698ec9feb89d7350db0a40193276
adcelis <adridclis@gmail.com>
2024-11-06 16:32:34 +0100

    atenderjugadores intercambia datos

--- src/trabajoDistri/AtenderJugadores.java
+++ src/trabajoDistri/AtenderJugadores.java
@@ -1,8 +1,7 @@
 package trabajoDistri;
 
-import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 public class AtenderJugadores extends Thread {
@@ -10,11 +9,36 @@ public class AtenderJugadores extends Thread {
 	private Socket s2;
 	public AtenderJugadores(Socket s1, Socket s2) {this.s1= s1;this.s2=s2;}
 	public void run() {
-		try
-		{
-			//hace que los dos jugadores se comuniquen entre ellos
-			// Prueba push oscar
+		try (
+            ObjectOutputStream out1 = new ObjectOutputStream(s1.getOutputStream());
+            ObjectOutputStream out2 = new ObjectOutputStream(s2.getOutputStream());
+        ) {
+            // Enviar host
+            out1.writeObject(s2.getInetAddress().getHostAddress());
+            out2.writeObject(s1.getInetAddress().getHostAddress());
+			// Enviar servidor
+			out1.writeObject(s2.getPort());
+            out2.writeObject(s1.getPort());
+
+            out1.flush();
+            out2.flush();
 		}
 		catch(IOException e) {e.printStackTrace();}
+		finally{
+			try {
+				if (s1 != null && !s1.isClosed()) {
+					s1.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				if (s2 != null && !s2.isClosed()) {
+					s2.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
